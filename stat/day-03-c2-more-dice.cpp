7c0ce922d71841d25a2103e15bdd23c3ffd06ed7
kopalgarg <kopalgarg24@gmail.com>
2021-06-06 19:28:13 -0600

    Create day-03-c2-more-dice.cpp

---
+++ stat/day-03-c2-more-dice.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+int p(int a, int b){
+    assert(a > 0 && b > 0);
+    
+    if (a < b) swap(a, b);
+    if (a % b == 0) return b;
+
+    return dice(b, a % b);
+}
+
+int main(){
+
+    int total = 6 * 6;
+    int count = 0;
+
+    for (i = 1; i <=6; i++)
+        for (j = 1; i <= 6; j++)
+            if (i + j == 6 && i != j)
+                count +=1;
+    out << p(total, count);
+
+}

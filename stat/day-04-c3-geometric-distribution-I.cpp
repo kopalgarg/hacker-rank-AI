90770d3b715539f6d70e037881e52165a82a2151
kopalgarg <kopalgarg24@gmail.com>
2021-06-09 21:51:19 -0600

    Create day-04-c3-geometric-distribution-I.cpp

---
+++ stat/day-04-c3-geometric-distribution-I.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    double p =  0.333; 
+    double q = 1-p;
+    int n = 5;
+    
+    double geometric_dist_probability = pow(q, n-1)*p; 
+    
+    printf("%.3f", geometric_dist_probability);
+    return 0;
+}

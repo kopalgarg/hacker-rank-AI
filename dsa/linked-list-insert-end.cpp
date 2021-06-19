23e287f7c968e7ed592399bac1a046593b35bc42
kopalgarg <kopalgarg24@gmail.com>
2021-06-18 22:52:32 -0600

    Create linked-list-insert-end.cpp

---
+++ dsa/linked-list-insert-end.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+        }
+
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+    // temp pointer pointing to a new node
+    SinglyLinkedListNode* temp = new SinglyLinkedListNode(data);
+    // set it to int data passed to the fcn
+    temp->data = data;
+    temp->next = NULL; 
+    // temp pointer to traverse through the list
+    SinglyLinkedListNode *ptr = head;
+    // check to see if empty linked list
+    if (head == NULL){
+        // set new node as head 
+        head = temp;
+        return head;
+    }
+    else {
+        // find end of linked list
+        while(ptr->next != NULL){
+            ptr = ptr->next;
+        }
+        // make the last element's head point to temp
+        ptr->next = temp;
+    }
+    return head;
+    
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+      	SinglyLinkedListNode* llist_head = insertNodeAtTail(llist->head, llist_item);
+        llist->head = llist_head;
+    }
+
+    print_singly_linked_list(llist->head, "\n", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist->head);
+
+    fout.close();
+
+    return 0;
+}

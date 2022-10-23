4fd25eb2b24ae1bf741095b0c0c26635e374993d
kopalgarg <kopalgarg24@gmail.com>
2022-10-22 19:44:58 -0600

    Update twoSum.py

--- arrays/twoSum.py
+++ arrays/twoSum.py
@@ -1,25 +1,11 @@
+# return the indices of numbers that sum up to target
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        # 1. brute force solution
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i]+nums[j] == target:
-                    return i,j
-
-        # 2. hashmap two-pass solution
         hashmap = {}
-        for i in range(len(nums)):
-            hashmap[nums[i]] = i # element-value is key and index is value
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap and hashmap[complement]!=i:
-                return [i, hashmap[complement]]
-
-        # 3. hashmap one-pass solution
-        hashmap = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
+        for i, num in enumerate(nums):
+            complement = target - num
             if complement in hashmap:
                 return [i, hashmap[complement]]
-            hashmap[nums[i]] = i
+            hashmap[num]=i
+            
+            

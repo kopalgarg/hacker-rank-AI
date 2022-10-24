5e9474e3c0407aa4ff3dd1f031893eff07075658
kopalgarg <kopalgarg24@gmail.com>
2022-10-23 22:11:01 -0600

    Update missingNumber.py

--- arrays/missingNumber.py
+++ arrays/missingNumber.py
@@ -1,6 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        hashset=set()
         for i in range(len(nums)+1):
             if i not in nums: 
                 return i

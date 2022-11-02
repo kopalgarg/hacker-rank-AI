0bccc6ab6e8792a804feda887224a32d12ed73c2
kopalgarg <kopalgarg24@gmail.com>
2022-11-02 12:25:06 -0400

    Create findDuplicate.py

---
+++ binary-search/findDuplicate.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        low = 1
+        high = len(nums)-1
+
+        while low <= high:
+            cur = (low+high)//2
+            count = 0
+            
+            for num in nums:
+                if num <= cur:
+                    count+=1
+            
+            if count > cur:
+                dup=cur
+                high=cur-1
+            else:
+                low=cur+1
+        return dup
+                
+            
+            
+            
+            
